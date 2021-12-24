f6cc518f60e610d892f36dc123997ee637e9a00d
hadywk <78849657+hadywk@users.noreply.github.com>
2021-12-24 21:40:09 +0200

    worst fit

--- osproject.java
+++ osproject.java
@@ -31,6 +31,33 @@ public class Osproject {
               printResults(allocatedMemory,blockSize);
      
 }
+	static void worstFit(int blockSize[], int processSize[])
+	{
+            // assigning default value to all array elements
+            int m = blockSize.length;
+            int n = processSize.length;
+            int allocatedMemory[] = new int[m];
+            
+            for(int i =0;i<m;i++){
+                allocatedMemory[i]=-1;
+            }
+            
+            for(int i =0;i<n;i++){
+                int max=Integer.MIN_VALUE;
+                int index=-1;
+                for(int j=0;j<m;j++){
+                    if(processSize[i] <= blockSize[j] && allocatedMemory[j]==-1){
+                        if(blockSize[j]-processSize[i] >= max){
+                            max=blockSize[j]-processSize[i];
+                            index=j;
+                        }
+                    }
+                }
+                if(index!=-1)
+                    allocatedMemory[index] = processSize[i];   
+            }
+           printResults(allocatedMemory,blockSize);
+	}
 
      static void printResults(int allocatedMemory[], int blockSize[]){
              for(int i=0;i<allocatedMemory.length;i++){
